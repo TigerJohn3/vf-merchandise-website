b582d18a7e2528c77e9378d35127e5c3e87aac4f
TigerJohn3 <jhrabovsky3@gmail.com>
2021-11-16 08:39:08 -0700

    Changes up to building cart

---
+++ NOTES.md
@@ -0,0 +1,24 @@
+# Notes for user flow
+
+
+Product list
+
+|
+
+Product detail (add item to cart)
+
+|
+
+Cart view (order detail view) (all items in cart)
+
+|
+
+Checkout (need to put in address for shipping)
+
+|
+
+Payment (Now Payment Integration)
+
+|
+
+Order confirmation - Customer should have shipping info
--- ecom/urls.py
+++ ecom/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from core import views
 
@@ -9,9 +9,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomeView.as_view(), name='home'),
     path('contact/', views.ContactView.as_view(), name='contact'),
-
+    path('cart/', include('cart.urls', namespace='cart'))
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
